edb1795075f685561705ab36824f81cb5f844290
canyildiz <can.yildiz@commencis.com>
2024-03-21 10:27:58 +0300

    .

--- align/Program.cs
+++ align/Program.cs
@@ -78,11 +78,11 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Automatic migrations
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    dbContext.Database.Migrate();
-}
+//using (var scope = app.Services.CreateScope())
+//{
+//    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    dbContext.Database.Migrate();
+//}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
